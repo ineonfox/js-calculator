b082ad7d27064f5da53ca8e9086f75ea19fbb326
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-31 13:09:04 +0200

    Add basic calculator operations

---
+++ script.js
@@ -0,0 +1,25 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+function operate(string) {
+    string = string.replaceAll(' ', '');
+    console.log(string);
+    let [num1, operator, num2] = string.split('');
+    num1 = +num1;
+    num2 = +num2;
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
