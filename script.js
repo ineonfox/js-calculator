dd25c9aeecb55c9a195521a555fb1afdfeab160e
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-31 19:18:49 +0200

    Fix bugs

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
-
-const percent = (a, b) => a * (b / 100);
-
-function operate() {
-    addNumberToExpression();
-
-    let [num1, operator, num2] = expression.split(' ');
-    num1 = +num1;
-    num2 = +num2;
-    switch (operator) {
-        case '+':
-            currentNumber = add(num1, num2);
-            break;
-        case '-':
-            currentNumber = subtract(num1, num2);
-            break;
-        case '×':
-            currentNumber = multiply(num1, num2);
-            break;
-        case '÷':
-            currentNumber = divide(num1, num2);
-            break;
-        case '%':
-            currentNumber = percent(num1, num2);
-            break;
-    }
-    currentNumber = currentNumber.toString();
-    expression = `${currentNumber} ${operator} `;
-    updateScreen();
+const Operator = {
+    ADD: '+',
+    SUBTRACT: '–',
+    MULTIPLY: '×',
+    DIVIDE: '÷',
+    PERCENT: '%',
 }
 
 const unclearNumbers = document.querySelector('.calc-screen .unclear');
@@ -45,6 +15,77 @@ let expression = '';
 
 initializeScreen();
 
+const btnBackspace = document.querySelector('.backspace');
+btnBackspace.addEventListener('click', removeDigit);
+
+const btnClear = document.querySelector('.clear-result');
+btnClear.addEventListener('click', clearResult);
+
+const btnEquals = document.querySelector('.equal');
+btnEquals.addEventListener('click', operate);
+
+const btnSign = document.querySelector('.sign');
+btnSign.addEventListener('click', toggleSign);
+
+const btnDot = document.querySelector('.dot');
+btnDot.addEventListener('click', addDot);
+const dots = document.querySelector('.dots');
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const percent = (a, b) => a * (b / 100);
+
+function operate() {
+    if (!hasOperator()) {
+        return;
+    }
+    addNumberToExpression();
+    let [num1, operator, num2] = expression.split(' ');
+    num1 = +num1;
+    num2 = +num2;
+    switch (operator) {
+        case Operator.ADD:
+            currentNumber = add(num1, num2);
+            break;
+        case Operator.SUBTRACT:
+            currentNumber = subtract(num1, num2);
+            break;
+        case Operator.MULTIPLY:
+            currentNumber = multiply(num1, num2);
+            break;
+        case Operator.DIVIDE:
+            currentNumber = divide(num1, num2);
+            break;
+        case Operator.PERCENT:
+            currentNumber = percent(num1, num2);
+            break;
+    }
+    currentNumber = currentNumber.toString();
+    if ((getDigitsAmount(currentNumber) > TOTAL_DIGITS
+        && +currentNumber >= 10**TOTAL_DIGITS) 
+        || typeof(currentNumber) === NaN) {
+        currentNumber = 'ERROR';        
+    }
+    // if number is not too big, but too big to display
+    else if (getDigitsAmount(currentNumber) > TOTAL_DIGITS) {
+        let wholeNumber = getDigitsAmount(Math.trunc(+currentNumber).toString());
+        currentNumber = roundToDecimal(+currentNumber, TOTAL_DIGITS - wholeNumber).toString();
+    }
+    expression = '';
+    // expression = currentNumber;
+    updateScreen();
+}
+
+function roundToDecimal(num, dec) {
+    return Math.round(num * 10**dec) / 10**dec;
+}
+
 function initializeScreen() {
     const numbers = document.querySelectorAll('.number');
     for (let number of numbers) {
@@ -52,7 +93,6 @@ function initializeScreen() {
     }
 
     const operators = document.querySelectorAll('.operator');
-    console.log(operators)
     for (let operator of operators) {
         operator.addEventListener('click', addOperator);
     }
@@ -65,15 +105,6 @@ function initializeScreen() {
         dots.appendChild(childDot);
     }
 }
-
-const btnBackspace = document.querySelector('.backspace');
-btnBackspace.addEventListener('click', removeDigit);
-
-const btnClear = document.querySelector('.clear-result');
-btnClear.addEventListener('click', clearResult);
-
-const btnEquals = document.querySelector('.equal');
-btnEquals.addEventListener('click', operate);
 
 function clearResult() {
     currentNumber = '';
@@ -90,16 +121,40 @@ function removeDigit() {
 
 function addNumberToExpression() {
     if (currentNumber.slice(-1) === '.') {
-        expression += currentNumber.slice(0, -1);
+        expression += ' ' + currentNumber.slice(0, -1);
     }
     else {
-        expression += currentNumber;
+        expression += ' ' + currentNumber;
     }
+    expression = expression.trim();
+}
+
+function hasOperator() {
+    let operands = expression.split(' ');
+    return operands.some((v) => Object.values(Operator).includes(v));
+}
+
+function hasOperatorLast() {
+    let lastOperand = expression.split(' ').at(-1);
+    return Object.values(Operator).includes(lastOperand)
 }
 
 function addOperator(evt) {
-    addNumberToExpression();
-    expression += ` ${evt.target.textContent} `;
+    if (hasOperatorLast()) {
+        if (currentNumber) {
+            expression = '';
+            addNumberToExpression();
+            expression += ` ${evt.target.textContent}`;
+        }
+        else {
+            expression = expression.split(' ')[0];
+            expression += ` ${evt.target.textContent}`;
+        }
+    }
+    else {
+        addNumberToExpression();
+        expression += ` ${evt.target.textContent}`;
+    }
     currentNumber = '';
     updateScreen();
 }
@@ -153,34 +208,13 @@ function getDigitsAmount(num) {
     if (!num) {
         return 0;
     }
-    let digitsAmount = 1;
-
-    // We use Math.ceil when < 0, and Math.floor otherwise
-    if (+num < 0) {
-        while(Math.ceil(num / (10**digitsAmount)) !== 0) {
-            digitsAmount++;
-        }
-    }
-    else {
-        while(Math.floor(num / (10**digitsAmount)) !== 0) {
-            digitsAmount++;
-        }
-    }
-    return digitsAmount;
+    return num.replace('-', '').replace('.', '').length;
 }
 
 function fixUnclearText(result) {
     let digitsAmount = getDigitsAmount(result);
     unclearNumbers.textContent = UNCLEAR_DIGIT.repeat(TOTAL_DIGITS - digitsAmount);
 }
-
-function clear() {
-    resultNumbers.textContent = '';
-    fixUnclearText();
-}
-
-const btnSign = document.querySelector('.sign');
-btnSign.addEventListener('click', toggleSign);
 
 function toggleSign() {
     if (currentNumber && getDigitsAmount(currentNumber) < TOTAL_DIGITS) {
@@ -194,11 +228,6 @@ function toggleSign() {
         updateScreen();
     }
 }
-
-
-const btnDot = document.querySelector('.dot');
-btnDot.addEventListener('click', addDot);
-const dots = document.querySelector('.dots');
 
 function addDot() {
     if (currentNumber) {
